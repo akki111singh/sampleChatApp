6bf061084227d6bf0670d95fe4f65e0af2c0a5b1
Akhil1.Singh <Akhil1.singh@ril.com>
2023-01-31 00:42:17 +0530

    Initial Commit

---
+++ Messenger/MessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerApp.swift
+//  Messenger
+//
+//  Created by Akhil1 Singh on 31/01/23.
+//
+
+import SwiftUI
+
+@main
+struct MessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
